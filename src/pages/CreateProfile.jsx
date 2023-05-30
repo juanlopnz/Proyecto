12ba565b6be31a6a5c0327d01ca16faa189ed268
juanlopnz <jucalora17@gmail.com>
2023-05-29 20:13:57 -0500

    Actualización de create profile

--- src/pages/CreateProfile.jsx
+++ src/pages/CreateProfile.jsx
@@ -2,52 +2,87 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const CreateProfile = () => {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const [Username, setUsername] = useState('');
+  const [Email, setEmail] = useState('');
+  const [Password, setPassword] = useState('');
+  const [ConfirmPassword, setConfirmPassword] = useState('');
+  const [User, setUser] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleUsername = (Username) => {
+    setUsername(Username)
+    setUser({ ...User, Username: Username })
+  }
+  const handleEmail = (Email) => {
+    setEmail(Email)
+    setUser({ ...User, Email: Email })
+  }
+  const handlePassword = (Password) => {
+    setPassword(Password)
+    setUser({ ...User, Password: Password })
+  }
+  const handleConfirmPassword = (ConfirmPassword) => {
+    setConfirmPassword(ConfirmPassword)
+  } 
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Lógica para enviar los datos del perfil a través de una API o realizar otras acciones
-    console.log('Perfil creado:', { username, email, password, confirmPassword});
+    if(Password !== ConfirmPassword){
+      alert('Las contraseñas no coinciden')
+    }
+
+    if (!Username || !Email || !ConfirmPassword || !ConfirmPassword) return;
+
+    //fetch local
+    await fetch('http://localhost:4000/api/auth/new', {
+      method: 'POST',
+      body: JSON.stringify(User),
+      headers: { "Content-Type": "application/json" }
+    }).then(() => {
+      console.log('Perfil creado:', { Username, Email, Password, ConfirmPassword});
+    })
+
+    setUsername("")
+    setEmail("")
+    setPassword("")
+    setConfirmPassword("")
   };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
     <div className="bg-white rounded-lg shadow-md p-4 max-w-xs mx-auto">
       <h2 className="text-3xl font-semibold text-center mb-6">Crear Perfil</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <input
           type="text"
           placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={Username}
+          onChange={(e) => handleUsername(e.target.value)}
           className="w-full mb-2 p-2 border border-gray-300 rounded"
         />
         <input
           type="email"
           placeholder="Gmail"
-          value={fullName}
-          onChange={(e) => setEmail(e.target.value)}
+          value={Email}
+          onChange={(e) => handleEmail(e.target.value)}
           className="w-full mb-2 p-2 border border-gray-300 rounded"
         />
         <input
           type="password"
           placeholder="Contraseña"
-          value={email}
-          onChange={(e) => setPassword(e.target.value)}
+          value={Password}
+          onChange={(e) => handlePassword(e.target.value)}
           className="w-full mb-2 p-2 border border-gray-300 rounded"
         />
         <input
           type="password"
           placeholder="Confirmar contraseña"
-          value={password}
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={ConfirmPassword}
+          onChange={(e) => handleConfirmPassword(e.target.value)}
           className="w-full mb-4 p-2 border border-gray-300 rounded"
         />
         <Link to="/home">
-          <button
+          <button onClick={handleSubmit}
             type="submit"
             className="w-full bg-blue-500 text-white font-semibold py-2 px-4 rounded"
           >
